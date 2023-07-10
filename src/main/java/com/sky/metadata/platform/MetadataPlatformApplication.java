ebd55bdf85cdc1dd87d4ab71e698cd6100e1698b
周官宝 <zhouguanbao@192.168.1.2>
2023-07-10 21:41:40 +0800

    初始化

---
+++ src/main/java/com/sky/metadata/platform/MetadataPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sky.metadata.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/metadata/platform/MetadataPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.metadata.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
